6f7b4d95c8370131e3da7da8e4afcec0a1e28902
Patrick Cozzi <pjcozzi@gmail.com>
2013-09-21 15:00:08 -0400

    Weekend pedestiran count

--- Source/PedestrianCounts.js
+++ Source/PedestrianCounts.js
@@ -3,61 +3,55 @@ function createPedestrianCount(viewer) {
 	var primitives = scene.getPrimitives();
 	var ellipsoid = viewer.centralBody.getEllipsoid();
 
+// TODO: Potential UI for these.  Low priority.
+	var scale = 1.0 / 100.0;
+	var colors = {
+		averageWeekdayPedestrianActivity : Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(0.0, 1.0, 0.0, 0.5)),
+		averageWeekendPedestrianActivity : Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(1.0, 0.0, 0.0, 0.5))
+	}
+	
 	return function(cameras) {
 // TODO: something cool with radius!
 		var radius = 20.0;
 
-		var outlineInstances = [];
 		var extrusionInstances = [];
 
 		for (var k = 0; k < cameras.length; ++k) {
 			var camera = cameras[k];
-			
 		    var center = ellipsoid.cartographicToCartesian(Cesium.Cartographic.fromDegrees(camera.longitude, camera.latitude));
-		    var height = 0.0;
-// TODO: user control scale    
-		    var extrudedHeight = camera.averageWeekdayPedestrianActivity / 100.0;
-		    
-		    outlineInstances.push(new Cesium.GeometryInstance({
-		        geometry : new Cesium.CircleOutlineGeometry({
+
+		    var bottom = 0.0;
+		    var top = camera.averageWeekdayPedestrianActivity * scale;
+
+		    extrusionInstances.push(new Cesium.GeometryInstance({
+		        geometry : new Cesium.CircleGeometry({
 		            center : center,
 		            radius : radius,
-		            height: height,
-		            extrudedHeight: extrudedHeight,
-		            numberOfVerticalLines: 0,
-		            granularity : Cesium.Math.toRadians(6.0)
+		            height: bottom,
+		            extrudedHeight: top,
+		            vertexFormat : Cesium.PerInstanceColorAppearance.VERTEX_FORMAT
 		        }),
 		        attributes : {
-		            color : Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.WHITE)
+		            color : colors.averageWeekdayPedestrianActivity
 		        }
 		    }));
+		    
+		    bottom = top;
+		    top += camera.averageWeekendPedestrianActivity * scale;
 		    
 		    extrusionInstances.push(new Cesium.GeometryInstance({
 		        geometry : new Cesium.CircleGeometry({
 		            center : center,
 		            radius : radius,
-		            height: height,
-		            extrudedHeight: extrudedHeight,
+		            height: bottom,
+		            extrudedHeight: top,
 		            vertexFormat : Cesium.PerInstanceColorAppearance.VERTEX_FORMAT
 		        }),
 		        attributes : {
-		            color : Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(0.0, 1.0, 0.0, 0.5))
+		            color : colors.averageWeekendPedestrianActivity
 		        }
-		    }));
+		    }));		    
 		}
-
-		primitives.add(new Cesium.Primitive({
-		    geometryInstances : outlineInstances,
-		    appearance : new Cesium.PerInstanceColorAppearance({
-		        flat : true,
-		        renderState : {
-		            depthTest : {
-		                enabled : true
-		            },
-		            lineWidth : Math.min(4.0, scene.getContext().getMaximumAliasedLineWidth())
-		        }
-		    })
-		}));
 
 		primitives.add(new Cesium.Primitive({
 		    geometryInstances : extrusionInstances,
